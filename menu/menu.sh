70a70ff7d9dff3661e5e66986342da523d380263
pandawadigital <136258892+pandawadigital@users.noreply.github.com>
2023-06-12 20:25:25 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -322,8 +322,8 @@ case $opt in
 12) clear ; up ;;
 13) clear ; slow ;;
 99) clear ; clearcache ;;
-6969) clear ; wget https://raw.githubusercontent.com/Andyyuda/Ver3/main/update.sh && chmod +x update.sh && ./update.sh && rm -f /root/update.sh ;;
+6969) clear ; wget https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/update.sh && chmod +x update.sh && ./update.sh && rm -f /root/update.sh ;;
 0) clear ; menu ;;
-x) exit ;;
+x) exit 
 *) echo -e "" ; echo "Press any key to back exit" ; sleep 1 ; exit ;;
 esac
