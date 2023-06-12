3ca64e95eb8115483fc4b1f409bd215a0daf2b38
pandawadigital <136258892+pandawadigital@users.noreply.github.com>
2023-06-12 20:07:17 +0700

    Update up.sh

--- up.sh
+++ up.sh
@@ -52,22 +52,22 @@ rm -rf slow
 # download script
 cd /usr/bin
 # menu
-wget -O menu "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu.sh"
-wget -O genssl "https://raw.githubusercontent.com/Andyyuda/ver3/main/ssh/genssl.sh"
-wget -O menu-trial "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu-trial.sh"
-wget -O menu-vmess "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu-vmess.sh"
-wget -O menu-vless "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu-vless.sh"
+wget -O menu "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/menu/menu.sh"
+wget -O genssl "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/ssh/genssl.sh"
+wget -O menu-trial "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/menu/menu-trial.sh"
+wget -O menu-vmess "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/menu/menu-vmess.sh"
+wget -O menu-vless "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/menu/menu-vless.sh"
 wget -O running "https://raw.githubusercontent.com/artanodrop/v4/main/menu/running.sh"
-wget -O clearcache "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/clearcache.sh"
+wget -O clearcache "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/menu/clearcache.sh"
 wget -O menu-trgo "https://raw.githubusercontent.com/artanodrop/v4/main/menu/menu-trgo.sh"
-wget -O menu-trojan "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu-trojan.sh"
-wget -O cf "https://raw.githubusercontent.com/Andyyuda/ver3/main/cf.sh"
-wget -O slow "https://raw.githubusercontent.com/Andyyuda/ver3/main/slow.sh"
+wget -O menu-trojan "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/menu/menu-trojan.sh"
+wget -O cf "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/cf.sh"
+wget -O slow "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/slow.sh"
 
 # menu ssh ovpn
-wget -O menu-ssh "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu-ssh.sh"
-wget -O usernew "https://raw.githubusercontent.com/Andyyuda/ver3/main/ssh/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/Andyyuda/ver3/main/ssh/trial.sh"
+wget -O menu-ssh "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/menu/menu-ssh.sh"
+wget -O usernew "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/ssh/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/ssh/trial.sh"
 wget -O renew "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/renew.sh"
 wget -O hapus "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/hapus.sh"
 wget -O cek "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/cek.sh"
@@ -78,14 +78,14 @@ wget -O ceklim "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/ceklim.
 wget -O tendang "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/tendang.sh"
 
 # menu system
-wget -O menu-set "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/menu-set.sh"
+wget -O menu-set "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/menu/menu-set.sh"
 wget -O menu-domain "https://raw.githubusercontent.com/artanodrop/v4/main/menu/menu-domain.sh"
 wget -O add-host "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/add-host.sh"
 wget -O port-change "https://raw.githubusercontent.com/artanodrop/v4/main/port/port-change.sh"
 wget -O certv2ray "https://raw.githubusercontent.com/artanodrop/v4/main/xray/certv2ray.sh"
 wget -O menu-webmin "https://raw.githubusercontent.com/artanodrop/v4/main/menu/menu-webmin.sh"
 wget -O speedtest "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/speedtest_cli.py"
-wget -O about "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/about.sh"
+wget -O about "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/menu/about.sh"
 wget -O auto-reboot "https://raw.githubusercontent.com/artanodrop/v4/main/menu/auto-reboot.sh"
 wget -O restart "https://raw.githubusercontent.com/artanodrop/v4/main/menu/restart.sh"
 wget -O bw "https://raw.githubusercontent.com/artanodrop/v4/main/menu/bw.sh"
@@ -100,13 +100,13 @@ wget -O acs-set "https://raw.githubusercontent.com/artanodrop/v4/main/acs-set.sh
 
 wget -O sshws "https://raw.githubusercontent.com/artanodrop/v4/main/ssh/sshws.sh"
 wget -O status "https://raw.githubusercontent.com/artanodrop/v4/main/status.sh"
-wget -O menu-backup "https://raw.githubusercontent.com/Andyyuda/ver3/main/backup/menu-backup.sh"
-wget -O backup "https://raw.githubusercontent.com/Andyyuda/ver3/main/backup/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/Andyyuda/ver3/main/backup/restore.sh"
+wget -O menu-backup "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/backup/menu-backup.sh"
+wget -O backup "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/backup/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/backup/restore.sh"
 
 wget -O jam "https://raw.githubusercontent.com/artanodrop/v4/main/jam.sh"
-wget -q -O /usr/bin/up "https://raw.githubusercontent.com/Andyyuda/ver3/main/up.sh" && chmod +x /usr/bin/up
-wget -q -O /usr/bin/fix "https://raw.githubusercontent.com/Andyyuda/ver3/main/menu/fix.sh" && chmod +x /usr/bin/fix
+wget -q -O /usr/bin/up "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/up.sh" && chmod +x /usr/bin/up
+wget -q -O /usr/bin/fix "https://raw.githubusercontent.com/pandawadigital/vpn01.2/main/menu/fix.sh" && chmod +x /usr/bin/fix
 wget https://raw.githubusercontent.com/Andyvpn/Autoscript-by-azi/main/autoscript-ssh-slowdns-main/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
 
 chmod +x menu
